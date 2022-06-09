e5c419c8924f078435cfdbc65b7f7ca01daa3f09
semi-cloud <sksmsrkdtpal@naver.com>
2022-06-09 18:11:24 +0900

    :recycle: 코드 개선

--- Part1/test.py
+++ Part1/test.py
@@ -3,7 +3,4 @@
 # mylist에 들은 각 원소의 길이를 담은 리스트를 리턴하도록 solution 함수를 작성해주세요.
 
 def solution(mylist):
-    answer = []
-    for lis in mylist:
-        answer.append(len(lis))
-    return answer
+    return list(map(len, mylist))
