5be865ad76a8202cb8e6e77bbdde7e2d388aaf9a
semi-cloud <sksmsrkdtpal@naver.com>
2022-06-09 18:07:34 +0900

    add: 연습 문제

---
+++ Part1/test.py
@@ -0,0 +1,9 @@
+# 정수를 담은 이차원 리스트,
+# mylist 가 solution 함수의 파라미터로 주어집니다.
+# mylist에 들은 각 원소의 길이를 담은 리스트를 리턴하도록 solution 함수를 작성해주세요.
+
+def solution(mylist):
+    answer = []
+    for lis in mylist:
+        answer.append(len(lis))
+    return answer
