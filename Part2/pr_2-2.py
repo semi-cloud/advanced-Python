cc0c7c3fa7d8b8594878706f75b261a057625b7a
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-01 18:53:35 +0900

    :recycle: 코드 개선 1차 2-2

--- Part2/pr_2-2.py
+++ Part2/pr_2-2.py
@@ -1,11 +1,9 @@
-# base 진법으로 표기된 숫자를 10진법 숫자 출력 하기
+# n 진법으로 표기된 문자열을 10진법 숫자로 변환
 
 num, base = map(int, input().strip().split(' '))
-num = str(num)
-result, j = 0, 0
-for i in range(len(num) - 1, -1, -1):
-    result += int(num[i]) * (base ** j)
-    j += 1
+result = 0
+for idx, n in enumerate(str(num)[::-1]):
+    result += int(n) * (base ** idx)
 print(result)
 
 
