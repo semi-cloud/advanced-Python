58ff26b0e92e0ef1d744f1ab9e60039a5b3186de
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-01 18:54:55 +0900

    :recycle: 코드 개선 2차 2-2

--- Part2/pr_2-2.py
+++ Part2/pr_2-2.py
@@ -1,11 +1,7 @@
 # n 진법으로 표기된 문자열을 10진법 숫자로 변환
 
 num, base = map(int, input().strip().split(' '))
-result = 0
-for idx, n in enumerate(str(num)[::-1]):
-    result += int(n) * (base ** idx)
-print(result)
-
+print(int(num, base))            # int(x, base=10) : 진법 변환을 지원
 
 
 
