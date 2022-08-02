1ab57219975e758ca01b8a5837c799904c48439c
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-02 19:06:42 +0900

    :recycle: 코드 개선 4-3

--- Part4/pr_4-3.py
+++ Part4/pr_4-3.py
@@ -2,7 +2,7 @@
 
 def solution(mylist):
     arr = []
-    for num1, num2 in zip(mylist[:-1], mylist[1:]):  # [ 83, 48, 13, 4, 71, 11] - [ 48, 13, 4, 71, 11]
+    for num1, num2 in zip(mylist, mylist[1:]):  # 길이가 다른 리스트가 인자로 들어오는 경우, 길이가 짧은 쪽 까지만 이터레이션이 이루어짐
         arr.append(abs(num1 - num2))
     return arr
 
