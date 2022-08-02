eaa6f10bf802abd6c49a71d3bbe1860a7c6db9bc
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-02 18:41:17 +0900

    :recycle: 코드 개선 : zip

--- Part4/pr_4-2.py
+++ Part4/pr_4-2.py
@@ -1,13 +1,9 @@
 # 2차원 리스트 행과 열 뒤집기
+# zip(*iterables) : 각 iterables 의 요소들을 모으는 이터레이터 반환, i번째 튜플은 i번째 원소들 포함(unpacking)
 
 def solution(mylist):
-    n = len(mylist)
-    answer = [[0] * n for _ in range(n)]
-    for i, row in enumerate(mylist):
-        for j, num in enumerate(row):
-            answer[j][i] = num
-    return answer
-
+    print(list(map(list, (zip(*mylist)))))    # (1,4,7) (2,5,8) (3,6,9)
 
 print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
 
