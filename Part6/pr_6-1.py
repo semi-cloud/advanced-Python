b13989a84d1baa2b22542c14ed1e4bec20820143
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-03 16:51:31 +0900

    :recycle: 코드 개선 1차 6-1

--- Part6/pr_6-1.py
+++ Part6/pr_6-1.py
@@ -2,9 +2,9 @@
 
 def solution(mylist):
     result = []
-    for lis in mylist:
-        for num in lis:
-            result.append(num)
+    for element in mylist:
+        result += element             # A + B : 배열 A와 B를 연결해 새로운 배열을 리턴
+        # result.append(element)      # 배열에 요소 추가 : [['A', 'B'], ['X', 'Y'], ['1']]
     return result
 
 print(solution([['A', 'B'], ['X', 'Y'], ['1']]))
