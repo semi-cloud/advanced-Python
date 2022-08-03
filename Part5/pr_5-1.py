c4673d00fdb1ad44c6f56684cb886df0c266fd3f
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-03 16:27:06 +0900

    :recycle: 코드 개선 5-1

--- Part5/pr_5-1.py
+++ Part5/pr_5-1.py
@@ -1,9 +1,6 @@
 # 리스트의 원소를 모두 이어붙인 문자열을 리턴
 
 def solution(mylist):
-    str = ""
-    for num in mylist:
-        str += num
-    return str
+    return ''.join(mylist)
 
-solution(['1', '100', '33'])
+print(solution(['1', '100', '33']))
