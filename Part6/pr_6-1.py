5ee3b4916444a18a3fbe758a482ea32cc362fdbf
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-03 17:05:30 +0900

    :recycle: 코드 개선 2차 6-1 : itertools, reduce

--- Part6/pr_6-1.py
+++ Part6/pr_6-1.py
@@ -1,10 +1,30 @@
 # 이차원을 일차원 리스트로 만들기
 
+import itertools
+from functools import reduce
+import operator
+# reduce : 반복 가능한 객체(iterable object) 내 각 요소를 연산한 뒤 이전 연산 결과들과 누적해서 반환
+
 def solution(mylist):
-    result = []
-    for element in mylist:
-        result += element             # A + B : 배열 A와 B를 연결해 새로운 배열을 리턴
-        # result.append(element)      # 배열에 요소 추가 : [['A', 'B'], ['X', 'Y'], ['1']]
+    # 방법 1 : sum
+    result = sum(mylist, [])
+
+    # 방법 2 - itertools.chain
+    result = list(itertools.chain.from_iterable(mylist))
+
+    # 방법 3 - itertools unpacking
+    result = list(itertools.chain(*mylist))
+
+    # 방법 4 - list comprehension 이용
+    result = [element for array in mylist for element in array]
+
+    # 방법 5 - reduce 함수 이용 1
+    result = list(reduce(lambda x, y: x + y, mylist))
+
+    # 방법 6 - reduce 함수 이용 2
+    result = list(reduce(operator.add, mylist))
+
     return result
+
 
 print(solution([['A', 'B'], ['X', 'Y'], ['1']]))
