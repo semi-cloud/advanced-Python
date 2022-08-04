7f005a2a1b5f9ded95b297aa17628db6e73e9149
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-04 17:31:05 +0900

    :recycle: 코드 개선 6-3 : counter

--- Part6/pr_6-3.py
+++ Part6/pr_6-3.py
@@ -1,10 +1,7 @@
 # 가장 많이 등장하는 알파벳만을 사전 순으로 출력
-from collections import defaultdict
+import collections
 
 my_str = input().strip()
-d = defaultdict(int)
-for char in my_str:
-    if char != "'":
-        d[char] += 1
-result = sorted([item[0] for item in d.items() if item[1] == max(d.values())])
-print("".join(result))
+result = collections.Counter(my_str)  # dict
+arr = sorted([item[0] for item in result.items() if item[1] == max(result.values())])
+print("".join(arr))
