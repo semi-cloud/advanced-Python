6abb3e110e8ecce5b3228c9615587c692945c3e1
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-05 14:19:13 +0900

    :recycle: 코드 개선 7-2

--- Part7/pr_7-2.py
+++ Part7/pr_7-2.py
@@ -2,15 +2,12 @@ import math
 
 arr = [int(input()) for _ in range(5)]
 result = 1
-flag = False
 for num in arr:
     result *= num
     if math.sqrt(result) == int(math.sqrt(result)):
-        flag = True
         print("found")
         break
-
-if not flag:
+else:                 # for-else : for 문이 중간에 break 등으로 끊기지 않고, 끝까지 수행 되었을 때 수행 하는 코드
     print("not found")
 
 
